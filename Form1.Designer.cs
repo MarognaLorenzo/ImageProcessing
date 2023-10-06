0f3f8c033cbca42e4e6a3dbb1767c70f6348ccbd
MarognaLorenzo <99410146+MarognaLorenzo@users.noreply.github.com>
2023-10-06 14:39:24 +0200

    Added buttons for choice task 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,16 +38,17 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.ApplyButton2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // LoadImageButton
             // 
-            this.LoadImageButton.Location = new System.Drawing.Point(16, 15);
-            this.LoadImageButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LoadImageButton.Location = new System.Drawing.Point(12, 12);
             this.LoadImageButton.Name = "LoadImageButton";
-            this.LoadImageButton.Size = new System.Drawing.Size(131, 28);
+            this.LoadImageButton.Size = new System.Drawing.Size(98, 23);
             this.LoadImageButton.TabIndex = 0;
             this.LoadImageButton.Text = "Load image...";
             this.LoadImageButton.UseVisualStyleBackColor = true;
@@ -61,29 +62,27 @@
             // 
             // imageFileName
             // 
-            this.imageFileName.Location = new System.Drawing.Point(155, 17);
-            this.imageFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.imageFileName.Location = new System.Drawing.Point(116, 14);
             this.imageFileName.Name = "imageFileName";
             this.imageFileName.ReadOnly = true;
-            this.imageFileName.Size = new System.Drawing.Size(420, 22);
+            this.imageFileName.Size = new System.Drawing.Size(316, 20);
             this.imageFileName.TabIndex = 1;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(17, 55);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 136);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(683, 630);
+            this.pictureBox1.Size = new System.Drawing.Size(493, 428);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+
             // 
             // applyButton
             // 
-            this.applyButton.Location = new System.Drawing.Point(630, -2);
-            this.applyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.applyButton.Location = new System.Drawing.Point(472, -2);
             this.applyButton.Name = "applyButton";
-            this.applyButton.Size = new System.Drawing.Size(132, 27);
+            this.applyButton.Size = new System.Drawing.Size(99, 22);
             this.applyButton.TabIndex = 3;
             this.applyButton.Text = "ImageB";
             this.applyButton.UseVisualStyleBackColor = true;
@@ -96,10 +95,9 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(1264, 14);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveButton.Location = new System.Drawing.Point(948, 11);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(127, 28);
+            this.saveButton.Size = new System.Drawing.Size(95, 23);
             this.saveButton.TabIndex = 4;
             this.saveButton.Text = "Save as BMP...";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -107,40 +105,59 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(708, 55);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(538, 136);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(683, 630);
+            this.pictureBox2.Size = new System.Drawing.Size(505, 428);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(783, 17);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Location = new System.Drawing.Point(649, 15);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(368, 25);
+            this.progressBar.Size = new System.Drawing.Size(276, 20);
             this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar.TabIndex = 6;
             this.progressBar.Visible = false;
             // 
             // ApplyButton2
             // 
-            this.ApplyButton2.Location = new System.Drawing.Point(630, 30);
-            this.ApplyButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.ApplyButton2.Location = new System.Drawing.Point(472, 24);
             this.ApplyButton2.Name = "ApplyButton2";
-            this.ApplyButton2.Size = new System.Drawing.Size(132, 27);
+            this.ApplyButton2.Size = new System.Drawing.Size(99, 22);
             this.ApplyButton2.TabIndex = 7;
             this.ApplyButton2.Text = "ImageC";
             this.ApplyButton2.UseVisualStyleBackColor = true;
             this.ApplyButton2.Click += new System.EventHandler(this.image_c_ButtonClick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Detect Areas (Flood Fill)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClickFloodFillButton);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 80);
+            this.button2.Name = "Choice function 2";
+            this.button2.Size = new System.Drawing.Size(99, 22);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Choice Function 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClickChoiceFunction2);
+            // 
             // INFOIBV
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1403, 709);
+            this.ClientSize = new System.Drawing.Size(1052, 576);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.ApplyButton2);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.pictureBox2);
@@ -150,7 +167,6 @@
             this.Controls.Add(this.imageFileName);
             this.Controls.Add(this.LoadImageButton);
             this.Location = new System.Drawing.Point(10, 10);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "INFOIBV";
             this.ShowIcon = false;
             this.Text = "INFOIBV";
@@ -173,6 +189,8 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Button ApplyButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -154,6 +154,81 @@ namespace INFOIBV
             pictureBox2.Image = (Image)OutputImage;                         // display output image
         }
 
+        private void ClickFloodFillButton(object sender, EventArgs e)
+        {
+            if (InputImage == null) return;                                 // get out if no input image
+            if (OutputImage != null) OutputImage.Dispose();                 // reset output image
+            OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // create new output image
+            Color[,] Image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // create array to speed-up operations (Bitmap functions are very slow)
+
+            // copy input Bitmap to array            
+            for (int x = 0; x < InputImage.Size.Width; x++)                 // loop over columns
+                for (int y = 0; y < InputImage.Size.Height; y++)            // loop over rows
+                    Image[x, y] = InputImage.GetPixel(x, y);                // set pixel color in array at (x,y)
+
+
+            byte[,] g_scale_image = convertToGrayscale(Image);          // convert image to grayscale
+
+            byte[,] thresholded = thresholdImage(g_scale_image, 127);
+
+            byte[,] labels = floodFill(thresholded);
+
+            byte[,] workingImage = adjustContrast(labels);
+
+
+            // ==================== END OF YOUR FUNCTION CALLS ====================
+            // ====================================================================
+
+            // copy array to output Bitmap
+            for (
+
+                int x = 0; x < workingImage.GetLength(0); x++)             // loop over columns
+                for (int y = 0; y < workingImage.GetLength(1); y++)         // loop over rows
+                {
+                    Color newColor = Color.FromArgb(workingImage[x, y], workingImage[x, y], workingImage[x, y]);
+                    OutputImage.SetPixel(x, y, newColor);                  // set the pixel color at coordinate (x,y)
+                }
+
+            pictureBox2.Image = (Image)OutputImage;                         // display output image
+        }
+
+        private void ClickChoiceFunction2(object sender, EventArgs e)
+        {
+            if (InputImage == null) return;                                 // get out if no input image
+            if (OutputImage != null) OutputImage.Dispose();                 // reset output image
+            OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // create new output image
+            Color[,] Image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // create array to speed-up operations (Bitmap functions are very slow)
+
+            // copy input Bitmap to array            
+            for (int x = 0; x < InputImage.Size.Width; x++)                 // loop over columns
+                for (int y = 0; y < InputImage.Size.Height; y++)            // loop over rows
+                    Image[x, y] = InputImage.GetPixel(x, y);                // set pixel color in array at (x,y)
+
+
+            // INSERT YOUR FUNCTION PIPELINE
+
+            byte[,] g_scale_image = convertToGrayscale(Image);          // convert image to grayscale
+
+            byte[,] thresholded = thresholdImage(g_scale_image, 127);
+
+            byte[,] labels = floodFill(thresholded);
+
+            byte[,] workingImage = adjustContrast(labels);
+
+
+            // copy array to output Bitmap
+            for (
+
+                int x = 0; x < workingImage.GetLength(0); x++)             // loop over columns
+                for (int y = 0; y < workingImage.GetLength(1); y++)         // loop over rows
+                {
+                    Color newColor = Color.FromArgb(workingImage[x, y], workingImage[x, y], workingImage[x, y]);
+                    OutputImage.SetPixel(x, y, newColor);                  // set the pixel color at coordinate (x,y)
+                }
+
+            pictureBox2.Image = (Image)OutputImage;                         // display output image
+        }
+
 
         /*
          * saveButton_Click: process when user clicks "Save" button
